b7e943d8c6fac75511bc0831c2a64d2c97b90ac1
Mogior <egormog00@bk.ru>
2023-01-05 00:21:38 +0300

    queue service

--- src/gate_way_service/blueprints/delete_rental_blueprint.py
+++ src/gate_way_service/blueprints/delete_rental_blueprint.py
@@ -3,7 +3,7 @@ import json
 import time
 
 from quart import Blueprint, Response, request
-from .service_requests import delete_data_from_service
+from .service_requests import delete_data_from_service, post_data_from_service
 
 delete_rental_blueprint = Blueprint('delete_rental', __name__, )
 
@@ -45,9 +45,16 @@ async def delete_rental(rentalUid: str) -> Response:
 
     while response is None:
         time.sleep(10)
-        response = delete_data_from_service(
-            'http://' + os.environ['PAYMENT_SERVICE_HOST'] + ':' + os.environ['PAYMENT_SERVICE_PORT']
-            + '/api/v1/payment/' + rental['paymentUid'], timeout=10)
+        response = post_data_from_service(
+            'http://' + os.environ['QUEUE_SERVICE_HOST'] + ':' + os.environ['QUEUE_SERVICE_PORT']
+            + '/api/v1/command_delete/', timeout=10,
+            data=json.dumps({
+                'url': (
+                        'http://' + os.environ['PAYMENT_SERVICE_HOST'] + ':' + os.environ['PAYMENT_SERVICE_PORT']
+                        + '/api/v1/payment/' + rental['paymentUid']
+                ),
+                'headers': {}
+            }))
 
     return Response(
         status=204
